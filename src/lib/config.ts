1eca045c5a65e90acf21f7caff6c9feb29d9a8ca
Srimal Fernando <srimal.fli@gmail.com>
2025-07-31 11:35:40 +0530

    layouts and components

---
+++ src/lib/config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://srimal.me",
+  title: "Srimal Fenando",
+  name: "Srimal Fernando",
+  ogImage: "/srimal-fernando.png",
+  description:
+    "Srimal Fernando is a software engineer and researcher with a passion for building innovative solutions. He specializes in web development, machine learning, and data science.",
+}
+
+// Need to add other links as well
+export const socialLinks = {
+  github: "https://github.com/srimalonline",
+  instagram: "https://instagram.com/srimalonline",
+  linkedin: "https://linkedin.com/in/srimal-online",
+  email: "mailto:hi@srimal.me",
+}
