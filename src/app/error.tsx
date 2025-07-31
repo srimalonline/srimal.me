0ad27d92cee3201f008dc24844013586c195d412
Srimal Fernando <srimal.fli@gmail.com>
2025-07-31 11:48:35 +0530

    error file

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react'
+
+export default function Error({error, reset}: {error: Error, reset: () => void}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <p>Ammatasiri ! Site ekata kelawela wage... refresh karala balanna...</p>
+    </div>
+  )
+}
